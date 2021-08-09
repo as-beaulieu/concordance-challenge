1e89d4f22002fd0266ecbdcccde6ef22127b7cf0
Andrew Beaulieu <as.beaulieu@gmail.com>
2021-08-09 16:44:35 -0600

    splitting and counting of words

--- main.go
+++ main.go
@@ -1,26 +1,79 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io/ioutil"
+	"os"
+	"strings"
 )
 
 func main() {
-	convert, err := fileToString("input.txt")
+	sentences, err := fileToString("input.txt")
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	fmt.Println(convert)
-}
+	fmt.Println(sentences)
 
-func fileToString(fileName string) (string, error) {
-	file, err := ioutil.ReadFile(fileName)
-	if err != nil {
-		return "", err
+	contents := make(map[string]details, 0)
+
+	for sentenceIndex, sentence := range sentences {
+		fmt.Printf("Reading sentence #%d: %v \n", sentenceIndex, sentence)
+		words := strings.Split(sentence, " ")
+		fmt.Printf("Reading words after space split: %v \n", words)
+		for _, word := range words {
+			lowerCaseWord := strings.ToLower(word)
+			d, exist := contents[lowerCaseWord]
+			if exist {
+				//found
+				fmt.Println("HEY I WAS FOUND!!!: ", lowerCaseWord)
+				d.count++
+				d.locations = append(d.locations, sentenceIndex+1)
+				contents[lowerCaseWord] = d
+			} else {
+				//not found
+				newDetails := details{
+					count:     1,
+					locations: []int{sentenceIndex + 1},
+				}
+				contents[lowerCaseWord] = newDetails
+			}
+		}
 	}
 
-	str := string(file)
+	fmt.Println(contents)
+}
 
-	return str, nil
+type details struct {
+	count     int
+	locations []int
+}
+
+func fileToString(fileName string) (sentences []string, err error) {
+	//file, err := ioutil.ReadFile(fileName)
+	//if err != nil {
+	//	return "", err
+	//}
+	//
+	//str := string(file)
+	//
+	//return str, nil
+
+	file, err := os.Open(fileName)
+	if err != nil {
+		return
+	}
+
+	defer file.Close()
+
+	s := bufio.NewScanner(file)
+	for s.Scan() {
+		r := s.Text()
+		if len(r) > 0 {
+			t := strings.Trim(r, ".")
+			sentences = append(sentences, t)
+		}
+	}
+
+	return
 }
